d880dbae8297768b07ed7420d9886648506d55eb
Franco <ufrancis186@gmail.com>
2025-10-16 02:54:22 +0100

    good night

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Herosection(){
     return(
         <div className="flex justify-center pt-32">
-            <p className="flex flex-row md:max-w-[650px] font-bold justify-center text-center md:text-5xl text-gray-800">Order from your Favourite Cafeteria. With Ease</p>
+            <p className="md:max-w-[750px] font-bold justify-center text-center md:text-5xl text-gray-800">Order from your Favourite Cafeteria.<span className="text-green-700 "> With Ease </span></p>
         </div>
     );
 }
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -4,6 +4,12 @@ function Homepage(){
     return(
         <div>
             <Herosection />
+
+            <div>
+                <div className="bg-black w-[200px] h-[250px] rounded-sm shadow-2xl shadow-gray-500">
+                    <p>Meals Delights</p>
+                </div>
+            </div>
         </div>
     );
 }
