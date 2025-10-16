13c8545d2dfe903c62c4ec09599e464c3fd41988
Franco <ufrancis186@gmail.com>
2025-10-16 02:29:50 +0100

    hero

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Logo from "../assets/Logo.jpg"
+import Navbar from "./Navbar";
 function Header(){
     return(
-        <div className="z-50 top-5 fixed justify-self-center items-center w-[85vw] backdrop-blur-sm">
+        <div className="z-50 top-5 flex flex-row fixed justify-self-center justify-between items-center w-[85vw] backdrop-blur-sm">
             <img src={Logo} className="w-[90px]" />
+            <Navbar />
         </div>
+
     );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 function Navbar(){
     return(
-        <div>
-            
+        <div className="">
+            <div className="flex flex-row gap-10 justify-center items-center pr-10">
+                <p className="font-bold cursor-pointer hover:text-gray-600 active:text-gray-800">Home</p>
+                <p className="font-bold cursor-pointer hover:text-gray-600 active:text-gray-800">Orders</p>
+                <p className="font-bold cursor-pointer hover:text-gray-600 active:text-gray-800">Profile</p>
+                <p className="font-bold cursor-pointer hover:text-gray-600 active:text-gray-800">Help</p>
+            </div>
         </div>
     );
 }
