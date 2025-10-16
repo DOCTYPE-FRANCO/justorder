d620ad2c35f07e5ce7b9f0a08502da27ecee33c6
Franco <ufrancis186@gmail.com>
2025-10-16 17:04:32 +0100

    later

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 function Herosection(){
     return(
         <div className="flex justify-center pt-32">
-            <p className="md:max-w-[750px] font-bold justify-center text-center md:text-5xl text-gray-800">Order from your Favourite Cafeteria.<span className="text-green-700 "> With Ease </span></p>
+            <p className="md:max-w-[750px] text-4xl max-w-[350px] font-bold justify-center text-center md:text-5xl text-gray-800">Order from your Favourite Cafeteria.<span className="text-green-700 "> With Ease </span></p>
         </div>
     );
 }
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -11,34 +11,30 @@ function Homepage(){
 
             <div className="flex justify-center mt-24 text-4xl text-green-700 font-bold ">Campus Cafeterias </div>
 
-            <div className="flex flex-col md:flex-row gap-10 justify-center mt-10">
-                <div className="flex flex-col gap-2 bg-white justify-center items-center w-[200px] h-[260px] rounded-sm shadow-2xl shadow-gray-500">
+            <div className="flex flex-col md:flex-row gap-10 justify-center items-center mt-10">
+                <div className="flex flex-col gap-2 bg-white justify-center items-center w-[270px] h-[350px] md:w-[200px] md:h-[280px] rounded-sm shadow-2xl shadow-gray-500">
                     <div className="w-[65%] h-[62%] rounded-xl overflow-hidden">
                         <img src={MealsDelight} className="fit hover:scale-110 transition-all duration-300" />
                     </div>
-                    <p className="text-xl font-bold">Meals Delights</p>
-                    <button className="bg-green-600 w-[100px] h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
+                    <p className="text-xl  font-bold">Meals Delights</p>
+                    <button className="bg-green-600 w-[150px] h-[30px] md:w-[100px] md:h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
                 </div>
 
-                <div className="flex flex-col gap-2 bg-white justify-center items-center w-[200px] h-[260px] rounded-sm shadow-2xl shadow-gray-500">
+                <div className="flex flex-col gap-2 bg-white justify-center items-center w-[270px] h-[350px] md:w-[200px] md:h-[280px] rounded-sm shadow-2xl shadow-gray-500">
                     <div className="w-[65%] h-[62%] rounded-xl overflow-hidden">
                         <img src={Heritage} className="fit hover:scale-110 transition-all duration-300" />
                     </div>
                     <p className="text-xl font-bold">Heritage</p>
-                    <button className="bg-green-600 w-[100px] h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
+                    <button className="bg-green-600 w-[150px] h-[30px] md:w-[100px] md:h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
                 </div>
 
-                <div className="flex flex-col gap-2 bg-white justify-center items-center w-[200px] h-[260px] rounded-sm shadow-2xl shadow-gray-500">
+                <div className="flex flex-col gap-2 bg-white justify-center items-center w-[270px] h-[350px] md:w-[200px] md:h-[280px] rounded-sm shadow-2xl shadow-gray-500">
                     <div className="w-[65%] h-[62%] rounded-xl overflow-hidden">
                         <img src={HisGrace} className="fit hover:scale-110 transition-all duration-300" />
                     </div>
                     <p className="text-xl font-bold">His Grace</p>
-                    <button className="bg-green-600 w-[100px] h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
-                </div>
-
-                
-
-                
+                    <button className="bg-green-600 w-[150px] h-[30px] md:w-[100px] md:h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
+                </div>    
             </div>
         </div>
     );
