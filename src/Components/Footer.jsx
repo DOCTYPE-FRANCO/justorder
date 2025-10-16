1892d942d1f0a377758278f33676133a814b3193
Franco <ufrancis186@gmail.com>
2025-10-16 16:14:42 +0100

    okay

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 function Footer(){
     return(
-        <div>
-            <p className="text-white ">&copy; 2025 Just Order By Franco. All rights reserved.</p>
-        </div>
+        <div className=" flex flex-col justify-center items-center w-full z-20 pt-20">
+            <p className="text-black font-bold">&copy; 2025 Just Order By Franco. All rights reserved.</p>
+        </div >
     );
 }
 
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -11,24 +11,34 @@ function Homepage(){
 
             <div className="flex justify-center mt-24 text-4xl text-green-700 font-bold ">Campus Cafeterias </div>
 
-            <div className="flex md:flex-row gap-10 justify-center mt-10">
+            <div className="flex flex-col md:flex-row gap-10 justify-center mt-10">
                 <div className="flex flex-col gap-2 bg-white justify-center items-center w-[200px] h-[260px] rounded-sm shadow-2xl shadow-gray-500">
-                    <img src={MealsDelight} className="w-[65%] rounded-xl" />
+                    <div className="w-[65%] h-[62%] rounded-xl overflow-hidden">
+                        <img src={MealsDelight} className="fit hover:scale-110 transition-all duration-300" />
+                    </div>
                     <p className="text-xl font-bold">Meals Delights</p>
                     <button className="bg-green-600 w-[100px] h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
                 </div>
 
                 <div className="flex flex-col gap-2 bg-white justify-center items-center w-[200px] h-[260px] rounded-sm shadow-2xl shadow-gray-500">
-                    <img src={HisGrace} className="w-[65%] rounded-xl" />
-                    <p className="text-xl font-bold">His Grace</p>
+                    <div className="w-[65%] h-[62%] rounded-xl overflow-hidden">
+                        <img src={Heritage} className="fit hover:scale-110 transition-all duration-300" />
+                    </div>
+                    <p className="text-xl font-bold">Heritage</p>
                     <button className="bg-green-600 w-[100px] h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
                 </div>
 
                 <div className="flex flex-col gap-2 bg-white justify-center items-center w-[200px] h-[260px] rounded-sm shadow-2xl shadow-gray-500">
-                    <img src={Heritage} className="w-[65%] rounded-xl" />
-                    <p className="text-xl font-bold">Heritage</p>
+                    <div className="w-[65%] h-[62%] rounded-xl overflow-hidden">
+                        <img src={HisGrace} className="fit hover:scale-110 transition-all duration-300" />
+                    </div>
+                    <p className="text-xl font-bold">His Grace</p>
                     <button className="bg-green-600 w-[100px] h-[25px] rounded-sm text-white mt-2 hover:scale-105 active:bg-gray-800 transition-all duration-500">View Menu</button>
                 </div>
+
+                
+
+                
             </div>
         </div>
     );
